0138b9ab3be31db9b6be463d98530190e4813712
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2024-09-25 14:42:48 -0300

    Postprocessing values changed by code

---
+++ Assets/PPCode.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PPCode : MonoBehaviour
+{
+    public PostProcessVolume volume;
+    private Vignette vignette;
+    private LensDistortion lens;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        volume.profile.TryGetSettings(out vignette);
+        volume.profile.TryGetSettings(out lens);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            vignette.intensity.value = 0.7f;
+            lens.intensity.value = 85;
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            vignette.intensity.value = 0f;
+            lens.intensity.value = 0;
+        }
+    }
+}
